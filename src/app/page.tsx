7b48c11b86af85748b43c965fd97b4f054e336fb
TommyH <hiept81331@gmail.com>
2025-07-29 08:43:56 +0700

    test thử page tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,14 +6,19 @@ const { Title } = Typography;
 
 export default function Home() {
   return (
-    <div>
-      <Title level={2}>Hello Antd</Title>
-      <Button type="primary">Click me tran hiep</Button>
-      <div style={{ marginTop: 20 }}>
+    <div className="container">
+      <div>
+        <Title level={2}>Hello Antd</Title>
+        <Button type="primary">Click me tran hiep</Button>
         <Button type="default">Default Button</Button>
         <Button type="dashed" style={{ marginLeft: 8 }}>Dashed Button</Button>
         <Button type="text" style={{ marginLeft: 8 }}>Text Button</Button>
         <Button type="link" style={{ marginLeft: 8 }}>Link Button</Button>
+      </div>
+      <div className="footer">
+        <p>© 2023 My Ant Design App</p>
+        <Button type="link" style={{ marginLeft: 8 }}>Privacy Policy</Button>
+        <Button type="link" style={{ marginLeft: 8 }}>Terms of Service</Button>
       </div>
     </div>
   );
